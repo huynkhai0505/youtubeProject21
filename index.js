b55e856e7c75dc3fa10688540c4db56ecb3d84a2
huynkhai0505 <huynhkhai0505@gmail.com>
2020-09-02 11:30:39 +0700

    DOC

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+//set up express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT} `)
+});
+
+
+
